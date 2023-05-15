d0d7d611ebb3487297d08e0e8c08a5de18912ad8
DanteMiron <dantemiron@hotmail.com>
2023-05-15 09:32:04 -0300

    appDados

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,16 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'appDados';
+  dadoIzquierda = "../assets/img/dice1.png";
+  dadoDerecha = "../assets/img/dice2.png";
+  numero1:number= 1;
+  numero2:number= 2;
+
+  tirarDados():void{
+    this.numero1 = Math.round(Math.random()*5) + 1;
+    this.numero2 = Math.round(Math.random()*5) + 1;
+    this.dadoDerecha = `../assets/img/dice${this.numero2}.png`
+    this.dadoIzquierda = `../assets/img/dice${this.numero1}.png`
+  }
+
 }
